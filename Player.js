dc4077fcb5be36e7f55067d8b6f9572c84c2812c
Shrunaya <81635977+Shrunaya@users.noreply.github.com>
2021-04-16 21:27:01 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,21 @@
+class Player {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.spt = createSprite(this.x,this.y,50,50);
+        this.spt.shapeColor = "orange";
+    }
+    
+     moveLeft(){
+        this.spt.x = this.spt.x -10;
+    }
+    moveRight(){
+        this.spt.x = this.spt.x +10;
+    }
+    moveUp(){
+        this.spt.y = this.spt.y -10;
+    }
+    moveDown(){
+        this.spt.y = this.spt.y +10;
+    }
+}
